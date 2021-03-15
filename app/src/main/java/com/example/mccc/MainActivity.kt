f83a5375e701175f9e1a60df75abe5701d8c8683
AlaaZourob <dr.alaab@outlook.sa>
2021-03-15 20:32:04 +0200

    test added

--- app/src/main/java/com/example/mccc/MainActivity.kt
+++ app/src/main/java/com/example/mccc/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("az7","onCreat :Welcome")
+        Log.d("az7","onCreat :Welcome test")
+
     }
 }
